eddd92646b1505022cd788208a13ec0de073586a
wangchenhao006 <wangchenhao006@126.com>
2019-11-02 16:37:37 +0800

    feat: 可以托管到scrapy cloud，也可以部署在本地

---
+++ build/lib.linux-x86_64-2.7/tutorial/pipelines.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import pymysql
+import time,hashlib
+from twisted.enterprise import adbapi
+from pymysql import cursors
+
+class TutorialPipeline(object):
+    def process_item(self, item, spider):
+        return item
+
+def create_id():
+    m = hashlib.md5(str(time.clock()).encode('utf-8'))
+    return m.hexdigest()
+
+class NoveltySpiderPipeline(object):
+
+    def __init__(self):
+        dbparams = {
+            'host': '139.219.8.80',
+            'port': 4321,
+            'user': 'test',
+            'password': 'test',
+            'database': 'novelty',
+            'charset': 'utf8'
+        }
+        self.conn = pymysql.connect(**dbparams)
+        self.cursor = self.conn.cursor()
+        self._sql = None
+
+    def process_item(self, item, spider):
+        print(int(create_id(),16))
+        self.cursor.execute(self.sql, (item['cat'], item['detail'], item['title'], item['time'], item['pc'], item['note'],item['post_like'],item['thumb']))
+        self.conn.commit()
+        return item
+
+    @property
+    def sql(self):
+        if not self._sql:
+            self._sql = """
+                insert into T_article_list(F_cat,F_detail,F_title,F_time,F_pc,F_note,F_post_like,F_thumb) values(%s,%s,%s,%s,%s,%s,%s,%s)
+                """
+            return self._sql
+        return self._sql
---
+++ build/lib.linux-x86_64-2.7/tutorial/spiders/novelty.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from tutorial.items import NoveltyItem
+
+class NoveltySpider(scrapy.Spider):
+    name = 'novelty'
+    allowed_domains = ['fulibus.net']
+    start_urls = ['https://fulibus.net/']
+
+    def parse(self, response):
+        src = response.xpath("//div[@class='content-wrap']/div[@class='content'][1]/article")
+
+        for article in src:
+            item = NoveltyItem()
+            item['cat'] = article.xpath('header/a/text()').extract_first()
+            item['title'] = article.xpath('a/img/@alt').extract_first()
+            item['detail'] = article.xpath('a/@href').extract_first()
+            item['time'] = article.xpath('p[@class="meta"]/time/text()').extract_first()
+            item['pc'] = article.xpath('p[@class="meta"]/a[@class="pc"]/text()').extract_first()
+            item['post_like'] = article.xpath('p[@class="meta"]/a[@class="post-like"]/span/text()').extract_first()
+            item['note'] = article.xpath('p[@class="note"]/text()').extract_first()
+            item['thumb'] = article.xpath('a/img/@src').extract_first()
+            yield(item)
+            print(item)
+            print("--------\n")
+
+        next_page = response.xpath("//div[@class='content-wrap']/div[@class='content'][1]/div[@class='pagination']/ul/li[@class='next-page']/a/@href").extract_first()
+        print(next_page)
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse, dont_filter=True )
+        pass
